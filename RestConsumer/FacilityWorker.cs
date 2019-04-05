d522c8261a197ab8f88ef1c0a7f85837c0663def
Nikolai Holfelt Røssel <niko6307@edu.easj.dk>
2019-04-05 15:03:31 +0200

    Add project files.

---
+++ RestConsumer/FacilityWorker.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLibrary.Model;
+using Newtonsoft.Json;
+
+namespace RestConsumer
+{
+    class FacilityWorker
+    {
+        
+        
+
+            private const string URI = "http://localhost:62917/api/facilities";
+
+            public FacilityWorker()
+            {
+
+            }
+
+            public void Start()
+            {
+                List<Facility> facilities = GetAll();
+
+
+                foreach (var facility in facilities)
+                {
+                    Console.WriteLine("Facility:: " + facility);
+                }
+
+
+            }
+
+            private List<Facility> GetAll()
+            {
+                List<Facility> facilities = new List<Facility>();
+
+                using (HttpClient client = new HttpClient())
+                {
+                    Task<string> resTask = client.GetStringAsync(URI);
+                    string jsonStr = resTask.Result;
+
+                    facilities = JsonConvert.DeserializeObject<List<Facility>>(jsonStr);
+                }
+
+                return facilities;
+            }
+
+            private Facility GetOne(int id)
+            {
+                Facility facility = new Facility();
+
+                using (HttpClient client = new HttpClient())
+                {
+                    Task<string> resTask = client.GetStringAsync(URI + "/" + id);
+                    string jsonStr = resTask.Result;
+
+                    facility = JsonConvert.DeserializeObject<Facility>(jsonStr);
+                }
+
+                return facility;
+            }
+
+            private bool Delete(int id)
+            {
+                bool ok = true;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    Task<HttpResponseMessage> deleteAsync = client.DeleteAsync(URI + "/" + id);
+
+                    HttpResponseMessage resp = deleteAsync.Result;
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        string jsonStr = resp.Content.ReadAsStringAsync().Result;
+                        ok = JsonConvert.DeserializeObject<bool>(jsonStr);
+                    }
+                    else
+                    {
+                        ok = false;
+                    }
+                }
+
+                return ok;
+            }
+
+            private bool Post(Facility facility)
+            {
+                bool ok = true;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    string jsonStr = JsonConvert.SerializeObject(facility);
+                    StringContent content = new StringContent(jsonStr, Encoding.ASCII, "application/json");
+                    Task<HttpResponseMessage> postAsync = client.PostAsync(URI, content);
+
+                    HttpResponseMessage resp = postAsync.Result;
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        string jsonResStr = resp.Content.ReadAsStringAsync().Result;
+                        ok = JsonConvert.DeserializeObject<bool>(jsonStr);
+                    }
+                    else
+                    {
+                        ok = false;
+                    }
+                }
+
+                return ok;
+            }
+        
+    }
+}
---
+++ RestConsumer/Worker.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLibrary.Model;
+using Newtonsoft.Json;
+
+namespace RestConsumer
+{
+    internal class Worker
+    {
+
+        private const string URI = "http://localhost:62917/api/hotels";
+
+        public Worker()
+        {
+
+        }
+
+        public void Start()
+        {
+            List<Hotel> hotels = GetAll();
+
+
+            foreach (var hotel in hotels)
+            {
+                Console.WriteLine("Hotel:: " + hotel);
+            }
+
+
+        }
+
+        private List<Hotel> GetAll()
+        {
+            List<Hotel> hoteller = new List<Hotel>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI);
+                string jsonStr = resTask.Result;
+
+                hoteller = JsonConvert.DeserializeObject<List<Hotel>>(jsonStr);
+            }
+
+            return hoteller;
+        }
+
+        private Hotel GetOne(int id)
+        {
+            Hotel hotel = new Hotel();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI + "/" + id);
+                string jsonStr = resTask.Result;
+
+                hotel = JsonConvert.DeserializeObject<Hotel>(jsonStr);
+            }
+
+            return hotel;
+        }
+
+        private bool Delete(int id)
+        {
+            bool ok = true;
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<HttpResponseMessage> deleteAsync = client.DeleteAsync(URI + "/" + id);
+
+                HttpResponseMessage resp = deleteAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    string jsonStr = resp.Content.ReadAsStringAsync().Result;
+                    ok = JsonConvert.DeserializeObject<bool>(jsonStr);
+                }
+                else
+                {
+                    ok = false;
+                }
+            }
+
+            return ok;
+        }
+
+        private bool Post(Hotel hotel)
+        {
+            bool ok = true;
+
+            using (HttpClient client = new HttpClient())
+            {
+                string jsonStr = JsonConvert.SerializeObject(hotel);
+                StringContent content = new StringContent(jsonStr, Encoding.ASCII, "application/json");
+                Task<HttpResponseMessage> postAsync = client.PostAsync(URI, content);
+                 
+                HttpResponseMessage resp = postAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    string jsonResStr = resp.Content.ReadAsStringAsync().Result;
+                    ok = JsonConvert.DeserializeObject<bool>(jsonStr);
+                }
+                else
+                {
+                    ok = false;
+                }
+            }
+
+            return ok;
+        }
+    }
+}
